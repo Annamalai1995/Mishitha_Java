cc3f4e5ee924911c2729fe80875ad08259d800aa
Annamalai1995 <s.annamalai1995@gmail.com>
2025-08-13 17:27:44 +0530

    switch and nested

--- src/main/java/Core_java/controlstmts/Switchcase.java
+++ src/main/java/Core_java/controlstmts/Switchcase.java
@@ -1,7 +1,33 @@
 package Core_java.controlstmts;
 
+import java.util.Scanner;
+
 public class Switchcase {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Tell us Vowels letter");
+        String vowels=scan.next();
+        switch (vowels)
+        {
+            case "a":
+                System.out.println("A is Vowels ");
+                break;
+            case "e":
+                System.out.println(" E is Vowels");
+                break;
+            case  "i":
+                System.out.println("I is vowels");
+                break;
+            case "o":
+                System.out.println("O is vowels");
+                break;
+            case "u":
+                System.out.println(" U is Vowels");
+                break;
+            default:
+                System.out.println("NOT A VOWELS");
+
+        }
+
     }
 }
